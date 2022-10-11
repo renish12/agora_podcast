8592c314b7a0cb8a11301bb48e3eb79726300753
Renish Radadiya <dev.megaminds@gmail.com>
2022-10-11 09:34:58 +0530

    update config

--- App/config/agora.config.ts
+++ App/config/agora.config.ts
@@ -1,10 +1,10 @@
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: '38173ff6df724fa78daeb7e7f5f586a3',
+  appId: '53820971c19048ff9f9cd0ba9e5cf83a',
   // Please refer to https://docs.agora.io/en/Agora%20Platform/token
   token:
-    '007eJxTYLgumruGe3q9/9K88Ik84pMSIgTnBHz4M000RtHQ5IqTQLECg7GFoblxWppZSpq5kUlaorlFSmJqknmqeZppmqmFWaJxR7xdMsdG++Sqb9wMjFAI4jMxhIQwMAAAUqsc0A==',
-  channelId: 'TT',
+    '007eJxTYAisl/wm1Pt73ZrILd9D2iuVYjonR077kjSRyd1j/reGTCMFBlNjCyMDS3PDZENLAxOLtDTLNMvkFIOkRMtU0+Q0C+NEt9cuyUdt3JJjd1sxMEIhiM/BYGhoaGBkYGTEwAAA9vohAg==',
+  channelId: '11102022',
   uid: parseInt((new Date().getTime() + '').slice(4, 13), 10),
 };
 
